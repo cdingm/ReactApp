086ab2592d7242435b436513b8816d6cd32f7365
cdingm <m.linder1@gmx.de>
2023-12-13 12:00:47 +0100

    a

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet } from 'react-native';
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello, React Nativee!</Text>
+      <Text style={styles.text}>Hello, React Native!</Text>
     </View>
   );
 };
