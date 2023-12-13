8323281c68480045fdc96c4c69b966a471711daf
cdingm <m.linder1@gmx.de>
2023-12-13 16:11:06 +0100

    changes files again so they are 1-1 like the tutorial

--- README.md
+++
@@ -1,5 +0,0 @@
-# ReactApp
-
-This is my demo app i use to learn React Native.
-
-Android Studio is used for developing this.
--- app/index.js
+++ app/index.js
@@ -2,8 +2,8 @@ import { useState } from 'react';
 import { View, Text, ScrollView, SafeAreaView } from 'react-native';
 import { Stack, useRouter } from 'expo-router';
 
-import { COLORS, icons, images, SIZES } from './constants';
-import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from './components';
+import { COLORS, icons, images, SIZES } from '../constants';
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components';
 
 const Home = () => {
     const router = useRouter();
