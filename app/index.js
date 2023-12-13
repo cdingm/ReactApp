33127b7b19842b0632236f583343d697fd6eca4f
cdingm <m.linder1@gmx.de>
2023-12-13 16:26:24 +0100

    add buttons

--- app/index.js
+++ app/index.js
@@ -9,7 +9,33 @@ const Home = () => {
     const router = useRouter();
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
-            <Text>Hello</Text>
+            <Stack.Screen
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+                    ),
+                    headerTitle: ""
+                }}
+            />
+
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={{
+                    flex:1,
+                    padding: SIZES.medium
+                }}>
+                    <Welcome
+                    
+                    />
+                    <Popularjobs/>
+                    <Nearbyjobs/>
+                </View>
+            </ScrollView>
+
         </SafeAreaView>
     )
 }
